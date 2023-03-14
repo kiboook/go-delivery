4d9ed6b669c536923807beae38a4102a659d6aff
kibok <nkb7714@naver.com>
2023-03-15 01:07:51 +0900

    [#0-MODIFY] DB config 추가

--- config/database.go
+++ config/database.go
@@ -1,5 +1,33 @@
 package config
 
-func Connect() {
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
 
+var (
+	db *gorm.DB
+)
+
+func InitDB() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// 데이터베이스 연결 설정
+	dsn := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+}
+
+func GetDB() *gorm.DB {
+	return db
 }
